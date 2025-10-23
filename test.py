c333656b8ae4d256111fa1f6b72fee1e0b2af97f
Julien <julienlarivierechartier@gmail.com>
2025-10-23 21:38:16 +0000

    Most basic test.py working without GUI support

--- test.py
+++ test.py
@@ -1,21 +1,62 @@
-import gymnasium as gym
-from gymnasium.utils.env_checker import check_env
+"""-------------------------------------------------------------------------------------
+File: test.py
+Description: First test to see if the SUMO-RL custom environment and code pulled from
+GitHub can be used.
+-------------------------------------------------------------------------------------"""
 
+import gymnasium as gym
 import sumo_rl
 
+# Location of files for single intersection (adapt path to your sumo-rl installation)
+SINGLE_INTERSECTION_DIR = "/opt/sumo-rl/sumo_rl/nets/single-intersection/"
 
-def test_api():
+
+def test_sumo_rl():
+    """
+    Function to test the SUMO-RL library with single intersection network/route
+    files.
+    """
+
+    # Create the custom Gym Env
     env = gym.make(
         "sumo-rl-v0",
-        num_seconds=100,
+        num_seconds=200,
         use_gui=False,
-        net_file="sumo_rl/nets/single-intersection/single-intersection.net.xml",
-        route_file="sumo_rl/nets/single-intersection/single-intersection.rou.xml",
+        net_file=SINGLE_INTERSECTION_DIR + "single-intersection.net.xml",
+        route_file=SINGLE_INTERSECTION_DIR + "single-intersection.rou.xml",
     )
-    env.reset()
-    check_env(env.unwrapped, skip_render_check=True)
+
+    # Reset the environment, extract initial observation and information
+    obs, info = env.reset()
+    print(f"Initial observation: {obs}")
+    print(f"Initial info: {info}")
+
+    # Init the step index and terminated/truncated flags
+    step_index = 0
+    terminated = False
+    truncated = False
+
+    # Loop until episode is done
+    while not (terminated or truncated):
+
+        # Choose random action, take it and observe nenxt state, rewards, etc.
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        # Print step info
+        print(f"\n# Step {step_index}:")
+        print(f"Action: {action}")
+        print(f"Observation: {obs}")
+        print(f"Reward: {reward}")
+        print(f"Terminated: {terminated}")
+        print(f"Truncated: {truncated}")
+        print(f"Info: {info}")
+
+        # Increment step
+        step_index += 1
+
     env.close()
 
 
 if __name__ == "__main__":
-    test_api()
+    test_sumo_rl()
