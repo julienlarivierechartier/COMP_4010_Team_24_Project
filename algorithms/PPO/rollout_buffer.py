ae038445fe6684bd87641a77b687885e48c92500
Victor Wang <74549506+VitalzAqua@users.noreply.github.com>
2025-11-29 19:01:07 -0500

    Add reward and done storage to RolloutBuffer

--- algorithms/PPO/rollout_buffer.py
+++ algorithms/PPO/rollout_buffer.py
@@ -1,7 +1,11 @@
 import torch
+import numpy as np
 
 
 class RolloutBuffer:
+    # stores all the trajectory data for one episode
+    # PPO needs full episode before updating
+    
     def __init__(self):
         self.states = []
         self.actions = []
@@ -9,11 +13,27 @@ class RolloutBuffer:
         self.rewards = []
         self.dones = []
         self.values = []
+        
+        # computed later during training
+        self.advantages = []
+        self.returns = []
 
     def clear(self):
+        # reset buffer after update
         self.states = []
         self.actions = []
         self.logprobs = []
         self.rewards = []
         self.dones = []
         self.values = []
+        self.advantages = []
+        self.returns = []
+    
+    def size(self):
+        # how many timesteps we have stored
+        return len(self.states)
+    
+    def add_reward_and_done(self, reward, done):
+        # store reward and done flag after each step
+        self.rewards.append(reward)
+        self.dones.append(done)
